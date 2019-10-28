5cbfc7cea3df630e49367b0d9290f1fb33ddfb53
Victor Novais <victor96novais@gmail.com>
2019-10-28 14:57:26 -0300

    :rocket: desafio finalizado

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Logo from "../assets/facebook-1.png";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="https://i.imgur.com/KDIDiSE.png" />
+        <div>
+          <span>Meu perfil</span>
+          <i className="material-icons">account_circle</i>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
